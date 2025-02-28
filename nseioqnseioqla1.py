5b3a983edcc4a16fadb414b2d374e455b2929c1b
mateusortlieb <mateusortlieb@gmail.com>
2025-02-28 20:33:36 -0300

    Update nseioqnseioqla1.py

--- nseioqnseioqla1.py
+++ nseioqnseioqla1.py
@@ -9,4 +9,4 @@ with col1:
 with col2:
   st.text_input("Nome:")
   st.date_input("Insira sua data de nascimento:")
-  st.slider("Qual seu sexo?" masculino, femboy ,feminino)
+  sex = st.slider("Qual seu sexo?", masculino, feminino)
