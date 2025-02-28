43c7aac285fd521bdbeb8c120e372951e38d38aa
mateusortlieb <mateusortlieb@gmail.com>
2025-02-28 20:37:09 -0300

    Update nseioqnseioqla1.py

--- nseioqnseioqla1.py
+++ nseioqnseioqla1.py
@@ -9,5 +9,5 @@ with col1:
 with col2:
   st.text_input("Nome:")
   st.date_input("Insira sua data de nascimento:")
-  masc = st.slider("Quanto % masculino?", 0, 100)
+  masc = st.slider("Quanto % masculino?", feminino, masculino)
   fem = st.slider("Quanto % feminino?", 0, 100)
