c0c2125e85c7b94c54a58896ab7656b343d1dfaa
mateusortlieb <mateusortlieb@gmail.com>
2025-02-28 20:15:53 -0300

    Update nseioqnseioqla1.py

--- nseioqnseioqla1.py
+++ nseioqnseioqla1.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
-st.header("Cadastre seu aluno:")
+col1, col2, col3 = st.columns(3)
 
-st.letterbox("aoaao")
+with col1:
+  st.header("Cadastre seu aluno:")
+
+with col2:
+  st.text_input("Nome?")
